d2730a6f29c386514b9226cee1e81ee6704b2179
Daniela Auriema <daniela.auriema@yahoo.com.br>
2024-12-13 17:43:09 +0100

    fix wait for connection

--- test/bash-wait/test-connection.sh
+++ test/bash-wait/test-connection.sh
@@ -3,13 +3,23 @@
 @ bash_wait_for_connection
 
 + pull postgres container
-docker pull postgres
+docker pull postgres:14-alpine
+
++ pull nginx container
+docker pull nginx:alpine-slim
 
 + run postgres container
-docker run --rm --name postgres_test -p "15432:5432" -e POSTGRES_PASSWORD=password -d postgres
+docker run --rm --name postgres_test -p "15432:5432" -e POSTGRES_PASSWORD=password -d postgres:14-alpine
 
 + wait for connection
 bash_wait_for_connection "localhost:15432"
 
++ run nginx container
+docker run --rm --name nginx_test -p "81:80" -d nginx:alpine-slim
+
++ wait for uri
+bash_wait_for_uri "http://localhost:81"
+
 + stop postgres container
-docker stop postgres_test
+docker stop postgres_test nginx_test
+
